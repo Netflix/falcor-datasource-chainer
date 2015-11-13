f6c70acd38600abad32415932bd7003ceca62215
Michael Paulson <mpaulson@netflix.com>
2015-11-13 07:32:49 -0800

    Added the travis/gitignore/ and bithoundrc files.  Now its time to get all three running and adding their badges to our README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,10 @@
 var gulp = require('gulp');
-var jscs = require('gulp-jscs');
-var eslint = require('gulp-eslint');
+
+// Gulp build step require
+require('./build/build.style');
+require('./build/build.test');
 
 gulp.task('build', ['jscs', 'lint', 'lint-test']);
+gulp.task('dist', ['jscs', 'lint', 'lint-test', 'test-coverage']);
 gulp.task('default', ['build']);
 
-gulp.task('jscs', function _jscs() {
-    return gulp.src([
-            './src/**/*.js',
-            './gulpfile.js'
-        ]).
-        pipe(jscs()).
-        pipe(jscs.reporter());
-});
-
-gulp.task('lint', function _lint() {
-    return gulp.src(['src/**/*.js']).
-        pipe(eslint()).
-        pipe(eslint.format()).
-        pipe(eslint.failAfterError());
-});
-
-gulp.task('lint-test', function _lint() {
-    return gulp.src(['test/**/*.js']).
-        pipe(eslint({
-            globals: {
-                describe: false,
-                it: true
-            },
-            rules: {
-                'no-unused-expressions': [ 0 ]
-            }
-        })).
-        pipe(eslint.format()).
-        pipe(eslint.failAfterError());
-});
