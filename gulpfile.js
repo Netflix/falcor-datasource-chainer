cbcc461508ea9a1126ff3077322fce94ac28b2cb
Michael Paulson <mpaulson@netflix.com>
2015-11-14 10:33:22 -0800

    I have webpack generating the builds but I am not validating them now am i creating them with travis programmatically.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 // Gulp build step require
 require('./build/build.style');
 require('./build/build.test');
+require('./build/build.dist');
 
 gulp.task('build', ['jscs', 'lint', 'lint-test']);
 gulp.task('dist', ['jscs', 'lint', 'lint-test', 'test-coverage']);
